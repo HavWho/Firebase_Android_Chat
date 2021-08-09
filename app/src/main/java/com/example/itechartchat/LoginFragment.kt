ee9519ff58528114035259446d0901c2721a3866
HavWho <workplacer2019@gmail.com>
2021-08-09 11:22:45 +0300

    add text checking on email input field and sign up fragment

--- app/src/main/java/com/example/itechartchat/LoginFragment.kt
+++ app/src/main/java/com/example/itechartchat/LoginFragment.kt
@@ -1,10 +1,16 @@
 package com.example.itechartchat
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import java.util.regex.Pattern
 
 /**
  * A simple [Fragment] subclass.
@@ -15,17 +21,46 @@ import android.view.ViewGroup
 class LoginFragment : Fragment() {
 
     private lateinit var mBinding: LoginFragment
+    private lateinit var mEmailTextInput : TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        mEmailTextInput = view.findViewById(R.id.email_textinputedittext)
+        //isValidEditText(mEmailTextInput.text.toString().trim())
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return view
+    }
+
+    override fun onResume() {
+        var isTextChanged = false
+        mEmailTextInput.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                if (s.toString().length == 0 && isTextChanged == true) {
+                    mEmailTextInput.error = "Email is required!"
+                    mEmailTextInput.requestFocus()
+                }
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(s.toString().trim()).matches()) {
+                    mEmailTextInput.setError("Your email is invalid!")
+                    mEmailTextInput.requestFocus()
+                }
+                isTextChanged = true
+            }
+        })
+        //isValidEditText(mEmailTextInput.text.toString().trim())
+        super.onResume()
     }
 
     companion object {
-
         @JvmStatic
         fun newInstance() : LoginFragment {
             val fragment = LoginFragment()
---
+++ app/src/main/java/com/example/itechartchat/SignUpFragment.kt
@@ -0,0 +1,31 @@
+package com.example.itechartchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SignUpFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): SignUpFragment {
+            val fragment = SignUpFragment()
+            return fragment
+        }
+    }
+}
